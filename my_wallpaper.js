3e36da3dba0e698da611836918d44ce5a645fa57
CRUZMARI21 <140350193+CRUZMARI21@users.noreply.github.com>
2023-08-07 09:54:12 +1200

    test

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,7 +1,5 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
-
+let eyeSize = 5;
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -9,15 +7,57 @@ function setup_wallpaper(pWallpaper) {
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
   //Grid settings
-  pWallpaper.grid_settings.cell_width  = 200;
-  pWallpaper.grid_settings.cell_height = 200;
+  pWallpaper.grid_settings.cell_width  = 100;
+  pWallpaper.grid_settings.cell_height = 100;
   pWallpaper.grid_settings.row_offset  = 50;
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(400,300);
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+
+  background(220, 180, 20);
+  ellipse(56, 46, 55, 55);//eyeSize
+  describe('A white ellipse with black outline in middle of a gray canvas.');
+
+
+
+  function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(480,0, 180);
+    
+    ellipse(200, 180, 80)
+    fill(150, 70 , 85)
+    stroke(200,40,30)
+    
+    rect()
+  }}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
