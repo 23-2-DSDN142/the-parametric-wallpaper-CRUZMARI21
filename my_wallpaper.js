25c08f89aeaf21cac89b87a43527c831c542383e
CRUZMARI21 <140350193+CRUZMARI21@users.noreply.github.com>
2023-08-07 14:00:32 +1200

    phoebe added Heart

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,5 +1,3 @@
-//your parameter variables go here!
-let eyeSize = 5;
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -15,45 +13,14 @@ function setup_wallpaper(pWallpaper) {
 function wallpaper_background() {
   background(220,0,200);
 }
+function my_symbol(){
+  
+fill('black');
+noStroke();
+ellipse(67, 67, 20, 20);
+ellipse(83, 67, 20, 20);
+triangle(91, 73, 75, 95, 59, 73);
 
-  function draw() {
-    ellipse(10, 200, 80)
-    ellipse(100, 200, 80)
-    ellipse(200, 200, 80)
-    ellipse(300, 200, 80)
-    ellipse(400, 200, 80)
-    fill(150, 70 , 85)
-    stroke(200,40,30)
-    
-    rect(-40, 80, 55)
-    rect(40, 80, 55)
-    rect(140, 80, 55)
-    rect(240, 80, 55)
-    rect(340, 80, 55)
-    fill(`460,270,138`)
-     
-    ellipse(10, 200, 80)
-    ellipse(100, 200, 80)
-    ellipse(60, 200, 80)
-    ellipse(100, 200, 80)
-    ellipse(150,200,80)
-    ellipse(200,200,80)
-    ellipse(250, 200, 80)
-    ellipse(350,200,80)
-    
-    rect(500, 580, 125)
-    rect(840, 280, 65)
-    
-    ellipse(30, 20, 90)
-    ellipse(150, 20, 90)
-    ellipse(300,20,90)
-    ellipse(420,20,90)
-    
-    rect(-240, 90, 55)
-    rect(240, 180, 55)
-    rect(340, 280, 55)
-    rect(440, 380, 55)
-    rect(540, 480, 55)
 }
 
 
