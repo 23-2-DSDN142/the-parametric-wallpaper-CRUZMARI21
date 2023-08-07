e123a4b567fcdeb0c287895e2fd0367a6f868fdd
CRUZMARI21 <140350193+CRUZMARI21@users.noreply.github.com>
2023-08-07 16:07:48 +1200

    maria added stroke weight, circles and squares

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -3,6 +3,8 @@
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
+// pWallpaper.output_mode(GRID_WALLPAPER);
+ 
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
@@ -14,18 +16,53 @@ function setup_wallpaper(pWallpaper) {
 
 function wallpaper_background() {
   
-  background(220,0,200);
+  background(180,0,202);
 }
 
 function my_symbol(){
 
-  myHeart(0,0, 20) 
-  myHeart(50,50,20)
-  myHeart(110,110,20)  
+  
+
+
+  point(20, 25);
+  point(90, 120);
+  stroke('purple');
+  strokeWeight(10);
+  point(85, 75);
+  point(15, 35);
+  describe(
+    'Four points drawn on a gray canvas. Two are black and two are purple. The points form the corners of a square.'
+  );
+
+
+
+  
+  fill (30,29,49)
+  square (-16, 70, 34, 5);
+  describe('A square with a black outline and round edges on a gray canvas.'
+  );
+
+  square (85, -32, 34, 5);
+  describe('A square with a black outline and round edges on a gray canvas.'
+  );
+
+ fill(30,29,49)
+ ellipse(52,-18,35)
+ ellipse(-0.10, 35, 30)
+ ellipse(100,35, -31,30)
+ ellipse(50, 85, -35, -35)
+ ellipse(-50, 90, -28)
+
+  myHeart(0,0) 
+  myHeart(-50,-40)
+  myHeart(50,50)
+  myHeart(100,100)  
+  myHeart(150,150)
+  myHeart(150,55)
 
 }
 
-function myHeart(heartX, heartY, size){
+function myHeart(heartX, heartY){
  
   fill('black');
   noStroke();
